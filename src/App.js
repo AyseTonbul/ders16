fc74aa4794ea2f13f033a81b4683b019f174d712
Ayse Tonbul <aysetonbul77@hotmail.com>
2023-01-01 18:01:36 +0300

    Redux uygulaması öncesi kodlar yazıldı

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import TodoEklemeFormu from './Components/TodoEklemeFormu'
+import TodoListesi from './Components/TodoListesi'
+import TodoTamamlananSayisi from './Components/TodoTamamlananSayisi'
 
 function App() {
   return (
-    <div>
-      <p>App 1</p>
-    </div>
+    <div className='container bg-white p-4 mt-5'>
+			<h1>Yapılacaklar Listem</h1>
+			<TodoEklemeFormu />
+			<TodoListesi/>
+			<TodoTamamlananSayisi />
+		</div>
   );
 }
 
