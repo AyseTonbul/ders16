035cadc498b99b1a6cb39aa1586d1c9a43b754bc
Ayse Tonbul <aysetonbul77@hotmail.com>
2023-01-01 20:55:36 +0300

    örnek tamamlandı

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import React from "react";
+import useListeCek from "./useListeCek";
+
 
 function App() {
+  const [veri, veriGuncelle] = useListeCek("https://jsonplaceholder.typicode.com/todos")
+
   return (
-    <div>
-      <p>App 1</p>
-    </div>
+    <>
+      <ol>
+        { veri && veri.map( (is)=> {
+          return <li key={is.id}>{is.title}</li>
+        } ) }
+      </ol>
+      
+    </>
   );
 }
 
 export default App;
+
 
